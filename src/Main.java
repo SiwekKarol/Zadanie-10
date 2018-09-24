2df1615b2c3696a6d7e9f2977f54fd3555228058
KarolSiwek <siwek.karol1992@gmail.com>
2018-09-24 23:01:51 +0200

    Dodana pętla do while

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Początek pętli while");
         double i = 0;
         while (i < 31) {
             System.out.println(i/10);
             i++;
         }
 
+        System.out.println("Koniec pętli while");
+        System.out.println("Początek pętli do while");
+        double y = 0;
+        do{
+            System.out.println(y/10);
+            y++;
+        } while (y < 31);
+        System.out.println("Koniec pętli do while");
     }
 }
