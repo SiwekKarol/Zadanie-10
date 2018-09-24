8b28ee095621aa6596e45a9ccb6fe3a01e0fd5c1
KarolSiwek <siwek.karol1992@gmail.com>
2018-09-24 22:47:02 +0200

    Działa ale nie wiem czy zadanie jest wykonane poprawnie... W sensie tak jak miało być.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        double i = 0;
+        while (i < 31) {
+            System.out.println(i/10);
+            i++;
+        }
+
+    }
+}
